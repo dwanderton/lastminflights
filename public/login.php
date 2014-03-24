da86285d4f5ecea9d4f74c8a445c2575d9cfcda7
David <dwanderton@gmail.com>
2014-03-23 20:57:25 -0400

    now all parts of booking form submit to server. Next step create confirmation page.

--- public/login.php
+++ public/login.php
@@ -34,18 +34,20 @@
                 // submit any booking request and provide confirmation
                 if(isset($_POST['submittedform'])){
                     //$submit = query("INSERT INTO flightrequest (userid, class, type, depart, to, departdate, returndate, departtime, returntime, adults, seniors, children) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $_SESSION["id"], $_POST["class"], $_POST["type"], $_POST["departingfrom"], $_POST["goingto"], $_POST["departdate"], $_POST["returndate"], $_POST["departtime"], $_POST["returntime"], $_POST["adults"], $_POST["seniors"], $_POST["children"]);
-                    $submit = query("INSERT INTO flightrequest (userid, class, type, depart, adults, children, seniors, departdate, returndate, departtime, returntime) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $_SESSION["id"], $_POST["class"], $_POST["type"], $_POST["departingfrom"], $_POST["adults"], $_POST["children"], $_POST["seniors"], $_POST["departdate"], $_POST["returndate"], $_POST["departtime"], $_POST["returntime"]);
+                    $submit = query("INSERT INTO flightrequest (userid, class, type, depart, adults, children, seniors, departdate, returndate, departtime, returntime, goingto) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $_SESSION["id"], $_POST["class"], $_POST["type"], $_POST["departingfrom"], $_POST["adults"], $_POST["children"], $_POST["seniors"], $_POST["departdate"], $_POST["returndate"], $_POST["departtime"], $_POST["returntime"], $_POST["goingto"]);
                     
                     if ($submit === false)
                         {
                             apologize("username already exists!");
                         }
-                    $rows = query("SELECT LAST_INSERT_ID() AS id"); 
-                    $flightrequestid = $rows[0]["id"];
-                    render("home.php", ["title" => $flightrequestid]);
+                    $rows = query("SELECT LAST_INSERT_ID() AS id");
+                    $flightrequested = $rows[0];
+                    $flightrequestedid = $flightrequest["id"];
+                    render("home.php", ["title" => "Request Received", "flightrequested" => $flightrequested]);
                 } else {
                     redirect("/");
                 }
+                exit;
             }
         }
 
