cd69b23d0a972798a6cf200fd400d53339982a21
David <dwanderton@gmail.com>
2014-03-23 20:44:51 -0400

    posting to flightrequest almost complete - issue with the to field.

--- public/login.php
+++ public/login.php
@@ -31,8 +31,21 @@
                 // remember that user's now logged in by storing user's ID in session
                 $_SESSION["id"] = $row["id"];
 
-                // redirect to portfolio
-                redirect("/");
+                // submit any booking request and provide confirmation
+                if(isset($_POST['submittedform'])){
+                    //$submit = query("INSERT INTO flightrequest (userid, class, type, depart, to, departdate, returndate, departtime, returntime, adults, seniors, children) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $_SESSION["id"], $_POST["class"], $_POST["type"], $_POST["departingfrom"], $_POST["goingto"], $_POST["departdate"], $_POST["returndate"], $_POST["departtime"], $_POST["returntime"], $_POST["adults"], $_POST["seniors"], $_POST["children"]);
+                    $submit = query("INSERT INTO flightrequest (userid, class, type, depart, adults, children, seniors, departdate, returndate, departtime, returntime) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $_SESSION["id"], $_POST["class"], $_POST["type"], $_POST["departingfrom"], $_POST["adults"], $_POST["children"], $_POST["seniors"], $_POST["departdate"], $_POST["returndate"], $_POST["departtime"], $_POST["returntime"]);
+                    
+                    if ($submit === false)
+                        {
+                            apologize("username already exists!");
+                        }
+                    $rows = query("SELECT LAST_INSERT_ID() AS id"); 
+                    $flightrequestid = $rows[0]["id"];
+                    render("home.php", ["title" => $flightrequestid]);
+                } else {
+                    redirect("/");
+                }
             }
         }
 
