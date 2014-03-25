ab60e7a83ed3b9b7315a4f95086af2e65c6f53dc
David <dwanderton@gmail.com>
2014-03-25 12:30:28 -0400

    added more Lorem text to homepage

--- templates/home.php
+++ templates/home.php
@@ -118,7 +118,7 @@
         </form>
     </div>
     <div class="col-sm-6">
-    <p>Some Lorem Ipsum text here about the history of everybody and everything in the world.</p>
+    <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 </div>
 
